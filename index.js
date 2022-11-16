9299bf420376bb6d5ea8d6eaca96aa13b05b8a8d
Falodun Damilola <dpaulzs11@gmail.com>
2022-11-16 08:42:50 +0100

    deleted tytpescript

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const bill = document.querySelector('#bill').value;
-const people = document.querySelector('#people').value;
-const custom = document.querySelector('#custom');
-const tip5 = document.querySelector('#tip5').value.replace('%','');
-const tip10 = document.querySelector('#tip10').value.replace('%','');
-const tip15 = document.querySelector('#tip15').value.replace('%','');
-const tip20 = document.querySelector('#tip20').value.replace('%','');
-const tip25 = document.querySelector('#tip25').value.replace('%','');
+let bill = document.querySelector('#bill');
+let people = document.querySelector('#people');
+let custom = document.querySelector('#custom');
+const tip5 = Number(document.querySelector('#tip5').value.replace('%',''));
+const tip10 = Number(document.querySelector('#tip10').value.replace('%',''));
+const tip15 = Number(document.querySelector('#tip15').value.replace('%',''));
+const tip20 = Number(document.querySelector('#tip20').value.replace('%',''));
+const tip25 = Number(document.querySelector('#tip25').value.replace('%',''));
 const reset = document.querySelector('#reset');
+
+let billValue;
+let peopleCount;
+
+bill.addEventListener('change',() => {
+    billValue = Number(bill.value);
+    console.log(billValue)
+})
+
+people.addEventListener('change', () => {
+    peopleCount = Number(people.value)
+    console.log(peopleCount)
+})
 
 // Function to reset the bill
 reset.addEventListener('click', () => {
-    bill.value='';
-    people.value='';
+    bill.value = '0';
+    people.value = '0';
 })
 
-const billValue = Number(bill)
+
+// const checkResetState = () => {
+//     if ( bill.value && people.value ){
+//         reset.disabled = false  
+//     } else {
+//         reset.disabled = true;
+//     }
+// }
+
+// reset.addEventListener('keyup', checkResetState)
